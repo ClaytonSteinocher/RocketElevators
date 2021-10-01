9f05f261db3038e60c47e6dc0d3dd7e511708fb3
ClaytonSteinocher <claysteinocher@gmail.com>
2021-10-01 14:26:38 -0400

    Exporting to GitHub.

--- RocketElevatorAssets/assets/js/quote.js
+++ RocketElevatorAssets/assets/js/quote.js
@@ -1,50 +1,177 @@
-$("#buildingType").change(function() {
-            if ($(this).val() == "Residential;") {
-            $('#residentialFieldDivApartments').show();
-            $('#residentialField').attr('required', '');
-            $('#residentialField').attr('data-error', 'This field is required.');
-            }
-            else if ($(this).val() == "Commercial;") {
-                $('#commercialFieldDiv').show();
-                $('#commercialField').attr('required', '');
-                $('#commercialField').attr('data-error', 'This field is required.');
-            }
-        else if ($(this).val() == "Corporate;") {
-                $('#commercialFieldDiv').show();
-                $('#commercialField').attr('required', '');
-                $('#commercialField').attr('data-error', 'This field is required.');
-        }   
-            else if ($(this).val() == "Commercial;") {
-                $('#commercialFieldDiv').show();
-                $('#commercialField').attr('required', '');
-                $('#commercialField').attr('data-error', 'This field is required.');
-        };
-        $("#buildingType").trigger("change");
+console.log('Connected')
+
+
+$("#building-type").change(function() {
+//   var dropdownMenu = document.getElementById("buildingType")
+//   var hiddeninputs = document.getElementsByClassName ("hidden");
+        
+        if ($(this).val() == "residential") {
+            console.log("residential chosen")
+            $('#number-of-apartments').show();
+            $('#number-of-floors').show();
+            $('#number-of-basements').show();
+            $('#number-of-parking-spots').hide();
+            $('#number-of-corporations').hide();
+            $('#maximum-occupancy').hide();
+            $('#business-hours').hide();
+            $('#number-of-companies').hide();
+            
+        } else if ($(this).val() == "commercial") {
+            console.log("commercial chosen")
+            $('#number-of-floors').show();
+            $('#number-of-basements').show();
+            $('#number-of-companies').show();
+            $('#number-of-parking-spots').show();
+            $('#number-of-elevators').show();
+            $('#number-of-apartments').hide();
+            $('#maximum-occupancy').hide();
+            $('#business-hours').hide();
+            $('#number-of-corporations').hide();
+        
+        } else if ($(this).val() == "corporate") {
+            console.log("corporate chosen")
+            $('#number-of-floors').show();
+            $('#number-of-basements').show();
+            $('#number-of-companies').hide();
+            $('#number-of-parking-spots').show();
+            $('#number-of-elevators').hide();
+            $('#number-of-apartments').hide();
+            $('#maximum-occupancy').show();
+            $('#business-hours').hide();
+            $('#number-of-corporations').show();
+
+        } else if ($(this).val() == "hybrid") {
+            console.log("hybrid chosen")
+            $('#number-of-floors').show();
+            $('#number-of-basements').show();
+            $('#number-of-companies').show();
+            $('#number-of-parking-spots').show();
+            $('#number-of-elevators').hide();
+            $('#number-of-apartments').hide();
+            $('#maximum-occupancy').show();
+            $('#business-hours').hide();
+            $('#number-of-corporations').hide();
+        }
 });
 
-  $("#numberOfFloors").change(function(){
-    let x = numberOfFloors
-   
-  });
+function calc()
+{
+    var numElevaorCages = (document.getElementById(numElevaorCages).value)
+    var elevatornumber = (document.getElementById(elevatornumber).value)
+    let numElevaorCages = elevatornumber
+
+}
+// $("#numApartments, #numFloors").on("input", function() {
+//     // Grabbing the value
+//     var apartmentVal = $(this).val()
     
+//     // Running some complex calculations 
+//     let calc = apartmentVal * 1000
+
+//     // Displaying back into the browser
+//     $("#finalPrice").val(calc)
+// });
+
+// $("#numFloors").on("input", function() {
+//     // Grabbing the value
+//     var floorVal = $(this).val()
+//     let divide = apartmentVal / floorVal
+
+//     $("#elevatorUnitPrice").val(divide)
+// });
+
+// $("#numBasements").on("input", function() {
+//     // Grabbing the value
+//     let basementVal = $(this).val()
+
+// });
+
+// $("#numDistinctBusinesses").on("input", function() {
+//     // Grabbing the value
+//     let businessVal = $(this).val()
+
+// });
+
+// $("#numParkingSpots").on("input", function() {
+//     // Grabbing the value
+//     let parkingVal = $(this).val()
+
+// });
+
+// $("#numElevatorsCages").on("input", function() {
+//     // Grabbing the value of number of Elevator Cages
+//     let elevatorVal = $(this).val()
+//     // Printing it into the Number of Elevators field
+//     $("#elevatorNumber").val(elevatorVal)
+//     console.log('equal')
+// });
+
+// $("#numSeperateCompanies").on("input", function() {
+//     // Grabbing the value
+//     let companyVal = $(this).val()
+
+// });
+
+// $("#numCorporations").on("input", function() {
+//     // Grabbing the value
+//     let corporationVal = $(this).val()
+
+// });
+
+// $("#numOccupantsPerFloor").on("input", function() {
+//     // Grabbing the value
+//     let occupantVal = $(this).val()
+
+// });
+
+// $("#numHoursOfActivity").on("input", function() {
+//     // Grabbing the value
+//     let hoursVal = $(this).val()
+
+// });
+
+
+$("input[type=radio]").each(function () {
+    $(this).click(function () {
+        if ($(this).val() == "standard") {
+            let elevatorNum = $("#elevatorNum").val()
+            $("#elevatorUnitPrice").val(elevatorNum * 7565)
+        }
+    })
+})
+
+
+// quoteFormElements: [
+//   {
+//       type: "residential",
+//       inputFields: ["number-of-apartments", "number-of-floors", "number-of-basements"]
+//   },
+//   {
+//       type: "commercial",
+//       inputFields: ["number-of-floors", "number-of-basements","number-of-companies", "number-of-parking-spots", "number-of-elevators"]
+//   },
+//   {
+//       type: "corporate",
+//       inputFields: ["number-of-floors", "number-of-basements","number-of-parking-spots", "number-of-corporations", "maximum-occupancy"]
+//   },
+//   {
+//       type: "hybrid",
+//       inputFields: ["number-of-floors", "number-of-basements", "number-of-companies", "number-of-parking-spots", "maximum-occupancy", "business-hours"]
+//   }
+// ]
+
+// console.log ()
 
 
 
+
+
+
+// $("#numberOfFloors").change(function(){
+  //  let x = numberOfFloors
+  // x -=
+  //}); 
     
-    if ($(this).val() == "yes") {
-      $('#otherFieldGroupDiv').show();
-      $('#otherField1').attr('required', '');
-      $('#otherField1').attr('data-error', 'This field is required.');
-      $('#otherField2').attr('required', '');
-      $('#otherField2').attr('data-error', 'This field is required.');
-    } else {
-      $('#otherFieldGroupDiv').hide();
-      $('#otherField1').removeAttr('required');
-      $('#otherField1').removeAttr('data-error');
-      $('#otherField2').removeAttr('required');
-      $('#otherField2').removeAttr('data-error');
-    }
-    // $("#seeAnotherFieldGroup").trigger("change")
 
 
     // RESET ALL FIELDS FUNCTION:
